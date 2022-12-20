283bed59181198023e4543c8c454af5ac8a00d7a
Ignacio Damián González <ignaciodamiang@gmail.com>
2022-12-20 19:16:49 -0300

    Product Service Http
    https://platzi.com/clases/2897-angular-unit-testing-servicios/47483-product-service-http/
    minuto 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Calculator } from './calculator';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,16 +9,14 @@ import { ProductsComponent } from './components/products/products.component';
 import { PicoPreviewComponent } from './components/pico-preview/pico-preview.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductsComponent,
-    PicoPreviewComponent
-  ],
+  declarations: [AppComponent, ProductsComponent, PicoPreviewComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
