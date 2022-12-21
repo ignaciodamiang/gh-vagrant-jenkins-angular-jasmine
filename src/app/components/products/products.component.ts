2538a5c4102ebbdc42b4a8e3304c098847169b3a
Ignacio Damián González <ignaciodamiang@gmail.com>
2022-12-20 22:50:41 -0300

    Product Service Http
    mock up and consume api

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from 'src/app/services/products.service';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
+  products: Product[] = [];
+  constructor(private productsService: ProductsService) {}
 
   ngOnInit(): void {
+    this.getAllProducts();
   }
 
+  getAllProducts() {
+    this.productsService.getAllSimple().subscribe((products) => {
+      this.products = products;
+    });
+  }
 }
